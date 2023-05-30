d00096e79ed7b1094f760fa2a03fc8ac6d6ba2f2
Charlie <charlietunguyen@hotmail.com>
2023-05-30 16:26:24 +0100

    Refactor login view into seperate components

--- InstagramSwiftUI/Core/Authentication/LoginView.swift
+++ InstagramSwiftUI/Core/Authentication/LoginView.swift
@@ -9,95 +9,123 @@ struct LoginView: View {
 			VStack {
 				Spacer()
 
-				Image("instagram-text-logo")
-					.resizable()
-					.scaledToFill()
-					.frame(width: 220, height: 100)
+				instagramLogo
 
 				VStack {
-					TextField("Enter your email", text: $email)
-						.textInputAutocapitalization(.never)
-						.font(.subheadline)
-						.padding(12)
-						.background(Color(.systemGray6))
-						.cornerRadius(10)
-						.padding(.horizontal, 24)
-
-					SecureField("Enter your password", text: $password)
-						.font(.subheadline)
-						.padding(12)
-						.background(Color(.systemGray6))
-						.cornerRadius(10)
-						.padding(.horizontal, 24)
+					emailTextField
+					passwordTextField
 				}
 
-				Button {
-
-				} label: {
-					Text("Forgot Password?")
-						.font(.footnote)
-						.fontWeight(.semibold)
-						.padding(.top)
-						.padding(.trailing, 24)
-				}
-				.frame(maxWidth: .infinity, alignment: .trailing)
-
-				Button {
-
-				} label: {
-					Text("Login")
-						.font(.subheadline)
-						.fontWeight(.semibold)
-						.foregroundColor(.white)
-						.frame(width: 360, height: 44)
-						.background(Color(.systemBlue))
-						.cornerRadius(8)
-				}
-				.padding(.vertical)
-
-				HStack {
-					Rectangle()
-						.frame(width: (UIScreen.main.bounds.width / 2) - 40, height: 0.5)
-
-					Text("OR")
-						.font(.footnote)
-						.fontWeight(.semibold)
-
-					Rectangle()
-						.frame(width: (UIScreen.main.bounds.width / 2) - 40, height: 0.5)
-				}
-				.foregroundColor(.gray)
-
-				HStack {
-					Image("facebook-logo")
-						.resizable()
-						.frame(width: 20, height: 20)
-
-					Text("Continue with Facebook")
-						.font(.footnote)
-						.fontWeight(.semibold)
-						.foregroundColor(Color(.systemBlue))
-				}
-				.padding(.top, 8)
+				forgotPasswordButton
+				loginButton
+				dividerComponent
+				continueWithFacebookButton
 
 				Spacer()
 
 				Divider()
 
-				NavigationLink {
-					Text("Sign up")
-				} label: {
-					HStack(spacing: 4) {
-						Text("Don't have an account?")
-
-						Text("Sign up")
-							.fontWeight(.semibold)
-					}
-					.font(.footnote)
-				}
-				.padding(.vertical, 16)
+				signUpNavLink
 			}
 		}
+	}
+
+	var instagramLogo: some View {
+		Image("instagram-text-logo")
+			.resizable()
+			.scaledToFill()
+			.frame(width: 220, height: 100)
+	}
+
+	var emailTextField: some View {
+		TextField("Enter your email", text: $email)
+			.textInputAutocapitalization(.never)
+			.font(.subheadline)
+			.padding(12)
+			.background(Color(.systemGray6))
+			.cornerRadius(10)
+			.padding(.horizontal, 24)
+	}
+
+	var passwordTextField: some View {
+		SecureField("Enter your password", text: $password)
+			.font(.subheadline)
+			.padding(12)
+			.background(Color(.systemGray6))
+			.cornerRadius(10)
+			.padding(.horizontal, 24)
+	}
+
+	var forgotPasswordButton: some View {
+		Button {
+
+		} label: {
+			Text("Forgot Password?")
+				.font(.footnote)
+				.fontWeight(.semibold)
+				.padding(.top)
+				.padding(.trailing, 24)
+		}
+		.frame(maxWidth: .infinity, alignment: .trailing)
+	}
+
+	var loginButton: some View {
+		Button {
+
+		} label: {
+			Text("Login")
+				.font(.subheadline)
+				.fontWeight(.semibold)
+				.foregroundColor(.white)
+				.frame(width: 360, height: 44)
+				.background(Color(.systemBlue))
+				.cornerRadius(8)
+		}
+		.padding(.vertical)
+	}
+
+	var dividerComponent: some View {
+		HStack {
+			Rectangle()
+				.frame(width: (UIScreen.main.bounds.width / 2) - 40, height: 0.5)
+
+			Text("OR")
+				.font(.footnote)
+				.fontWeight(.semibold)
+
+			Rectangle()
+				.frame(width: (UIScreen.main.bounds.width / 2) - 40, height: 0.5)
+		}
+		.foregroundColor(.gray)
+	}
+
+	var continueWithFacebookButton: some View {
+		HStack {
+			Image("facebook-logo")
+				.resizable()
+				.frame(width: 20, height: 20)
+
+			Text("Continue with Facebook")
+				.font(.footnote)
+				.fontWeight(.semibold)
+				.foregroundColor(Color(.systemBlue))
+		}
+		.padding(.top, 8)
+	}
+
+	var signUpNavLink: some View {
+		NavigationLink {
+			Text("Sign up")
+		} label: {
+			HStack(spacing: 4) {
+				Text("Don't have an account?")
+
+				Text("Sign up")
+					.fontWeight(.semibold)
+			}
+			.font(.footnote)
+		}
+		.padding(.vertical, 16)
 	}
 }
 
