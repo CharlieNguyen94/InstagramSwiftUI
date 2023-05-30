92e886ee11dd43c6bde80344b19e9b3d2f4ba380
Charlie <charlietunguyen@hotmail.com>
2023-05-30 11:28:03 +0100

    Initial Commit

---
+++ InstagramSwiftUI/InstagramSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUIApp.swift
+//  InstagramSwiftUI
+//
+//  Created by Charlie Nguyen on 30/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
