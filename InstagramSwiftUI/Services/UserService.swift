fd114d21c7e55b937977487f81de3db86af1266d
Charlie <charlietunguyen@hotmail.com>
2023-05-31 16:47:17 +0100

    Fetch all users

---
+++ InstagramSwiftUI/Services/UserService.swift
@@ -0,0 +1,11 @@
+import Foundation
+import Firebase
+
+struct UserService {
+
+	static func fetchAllUsers() async throws -> [User] {
+		let snapshot = try await Firestore.firestore().collection("users").getDocuments()
+		let documents = snapshot.documents
+		return documents.compactMap { try? $0.data(as: User.self) }
+	}
+}
