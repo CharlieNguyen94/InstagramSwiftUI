7cb32216797bafd6c3f3fb92848b769d092bc6ce
Charlie <charlietunguyen@hotmail.com>
2023-05-31 15:50:18 +0100

    Upload user data to firestore

--- InstagramSwiftUI/Core/Authentication/Service/AuthService.swift
+++ InstagramSwiftUI/Core/Authentication/Service/AuthService.swift
@@ -1,5 +1,7 @@
 import Foundation
 import FirebaseAuth
+import FirebaseFirestoreSwift
+import Firebase
 
 class AuthService {
 
@@ -26,6 +28,7 @@ class AuthService {
 		do {
 			let result = try await Auth.auth().createUser(withEmail: email, password: password)
 			self.userSession = result.user
+			await self.uploadUserData(uid: result.user.uid, username: username, email: email)
 		} catch {
 			print(error.localizedDescription)
 		}
@@ -38,5 +41,11 @@ class AuthService {
 	func signOut() {
 		try? Auth.auth().signOut()
 		self.userSession = nil
+	}
+
+	private func uploadUserData(uid: String, username: String, email: String) async {
+		let user = User(id: uid, username: username, email: email)
+		guard let encodedUser = try? Firestore.Encoder().encode(user) else { return }
+		try? await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
 	}
 }
